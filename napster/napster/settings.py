e635ad92554ac239ef33a3cc8c8fff024c74fcd5
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-02-27 23:06:07 -0600

    Removed static file finders

--- napster/napster/settings.py
+++ napster/napster/settings.py
@@ -111,7 +111,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
